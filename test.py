a1c7eae983e3330e1badcb9c1d4dafb75367582f
NezBut <xyi12312354@mail.ru>
2023-11-24 23:19:38 +0700

    ого афигеть

--- test.py
+++ test.py
@@ -2,8 +2,11 @@ print('test file')
 
 def main():
 
-    print('test git main')
+    print('test git __main__')
 
 
 if __name__ == '__main__':
     main()
+    print()
+    w = 323232
+    print(w * 2)

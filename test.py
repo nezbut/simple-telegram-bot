e175ff06c1faa2292d933038e339e1efce8f6f6d
NezBut <xyi12312354@mail.ru>
2023-11-25 15:26:03 +0700

    add async functions

--- test.py
+++ test.py
@@ -1,22 +1,39 @@
+import aiohttp
 import asyncio
+from random import randint
 
-async def test1():
-    print('opa 1')
-    await asyncio.sleep(2)
-    print('opa 2')
+async def request(number: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'http://numbersapi.com/{number}') as response:
+            return await response.text()
 
-async def test2():
-    print('aaaaa')
-    print('aaaaa')
-    print('aaaaa')
-    print('aaaaa')
+async def while_true():
+    while True:
+        await asyncio.sleep(10)
+        num = str(randint(1, 1_000_000))
+        resp = await request(num)
+        print(f"\n{resp}")
 
+
+async def while_true2():
+    loop = asyncio.get_running_loop()
+    while True:
+        inp = await loop.run_in_executor(None, input, 'Укажите число: ')
+
+        if inp == 'exit':
+            raise KeyboardInterrupt
+
+        elif not inp.isdigit():
+            print('Это не число')
+            continue
+
+        print(await request(inp))
 
 async def main():
-    tasks = [test1(), test2()]
+    async with asyncio.TaskGroup() as g:
+        g.create_task(while_true())
+        g.create_task(while_true2())
 
-    await asyncio.gather(*tasks)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())

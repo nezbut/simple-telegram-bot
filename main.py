508983aabd501841f0fbeb8f8fa0a3f65dde7082
NezBut <xyi12312354@mail.ru>
2023-11-27 02:04:58 +0700

    created new command: randomnum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram_bot import TelegramBot, BotCommand
 from api_numbers import request_to_numbers_api
 from animals_photos import RandomPhotoAnimal
+from random import randint
 from os import getenv
 from dotenv import load_dotenv
 import asyncio
@@ -17,6 +18,11 @@ async def command(cmd: BotCommand):
 @bot.command(name='cats', aliases=('dogs', 'foxs'))
 async def command_random_photo_cats(cmd: BotCommand):
     await cmd.send_photo(await random_photos_animals.request(cmd.command_name))
+
+@bot.command(name='randomnum', aliases=('rnum',))
+async def command_random_description_number(cmd: BotCommand):
+    num = str(randint(1, 10_000_000))
+    await cmd.send_message(await request_to_numbers_api(num))
 
 async def main():
     loop = asyncio.get_running_loop()
